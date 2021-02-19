11183e20f929fd566f74fa3f81fb814189545133
clyfe <>
2021-02-20 00:28:16 +0200

    Add treemacs config

---
+++ +treemacs.el
@@ -0,0 +1,7 @@
+;;; $DOOMDIR/+cua.el -*- lexical-binding: t; -*-
+
+;; Treemacs config!
+
+(setq doom-themes-treemacs-theme "doom-colors")
+
+(add-hook! treemacs-mode (text-scale-adjust -1))
--- config.el
+++ config.el
@@ -71,4 +71,5 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+(load! "+treemacs")
 (load! "+bindings")
