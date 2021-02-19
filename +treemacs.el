1657566a6f2d58fa50fa84aa7f6f09387989281c
clyfe <>
2021-02-20 00:38:24 +0200

    Make treemacs thinner

--- +treemacs.el
+++ +treemacs.el
@@ -2,6 +2,7 @@
 
 ;; Treemacs config!
 
-(setq doom-themes-treemacs-theme "doom-colors")
+(setq doom-themes-treemacs-theme "doom-colors"
+      treemacs-width 25)
 
 (add-hook! treemacs-mode (text-scale-adjust -1))
