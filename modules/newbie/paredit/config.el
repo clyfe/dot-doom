93d4e56960b46c258f67a4613fd0170e5f2bd340
clyfe <>
2021-02-23 20:55:06 +0200

    Toggle newbie paredit based on major mode

--- modules/newbie/paredit/config.el
+++ modules/newbie/paredit/config.el
@@ -3,5 +3,13 @@
 (load! "mode")
 
 ;; Paredit simplified
-(add-hook! smartparens-mode
+
+(add-hook! emacs-lisp-mode
   (newbie-paredit-mode t))
+
+(add-hook! lisp-mode
+  (newbie-paredit-mode t))
+
+(if (fboundp 'clojure-mode)
+    (add-hook! clojure-mode
+      (newbie-paredit-mode t)))
